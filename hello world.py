a04637fa96ad0191ad73f5fd59849d4f7f97d9c5
Harshit Raj <iamrajharshit@gmail.com>
2022-04-16 17:14:26 +0530

    random

--- hello world.py
+++ hello world.py
@@ -1,1 +1,16 @@
-print("hello world")
+import random
+n= 20
+to_be_guessed=int(n*random.random())+1
+guess = 0
+while guess!= to_be_guessed:
+    guess = int(input("New number"))
+    if guess >0:
+        if guess> to_be_guessed:
+            print("number too large")
+        elif guess < to_be_guessed:
+            print("no too small")
+    else:
+        print("sorry that you're giving up")
+        break            
+else:
+    print("cong you made it")
