c825e93f8e208bfff05e97f577b6dbfe9c309d67
Harshit Raj <iamrajharshit@gmail.com>
2022-04-16 17:55:44 +0530

    loops

---
+++ For loop.py
@@ -0,0 +1,10 @@
+#boolean condition
+#int vlaue of the counting
+#incrementtation of counting variable
+# for <vaiable> in <range>:
+
+fruits= ["mango","gapes","apple"]
+
+for fruit in fruits:
+    print("current friut",fruit)
+    
---
+++ for loop to cal factorial.py
@@ -0,0 +1,20 @@
+#3! (1x2x3)
+#5! (1x2x3x4x5)
+
+
+
+from re import I
+
+
+num= int(input("number:"))
+factorial=1
+
+if num<0:
+    print("number must be +ve")
+elif num==0:
+    print("factorial = 1")
+else:
+    for i in range(1,num+1):
+        factorial = factorial*i
+print(factorial)    
+
