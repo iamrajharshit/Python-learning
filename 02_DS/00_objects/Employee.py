9be7e6819a9f5f485ea0f670b360ef2e0edf538d
Harshit Raj <iamrajharshit@gmail.com>
2024-03-30 03:02:21 +0530

    for youtube

--- 02_DS/00_objects/Employee.py
+++ 02_DS/00_objects/Employee.py
@@ -4,10 +4,10 @@ its a way of representing data in our programming languages.
 
 Example: Employee
 Employee has attributes, properties associated with it.
--name 
--age
--salary
+-name
+-location 
 -job title
+-salary
 our goal would be to represent all of the data in a single entity.
 
 With object, we can represent all of this (attributes) in a single object.
@@ -18,34 +18,3 @@ A class, we are going to call it, what we want to call our object.
 (In gen. we name the class same as the file name.)
 Object is a single instance of a Class.
 '''
-
-class Employee:
-    
-    def __init__(self,name,jobtitle,salary):
-        self.name = name
-        self.jobtitle = jobtitle
-        self.salary = salary
-    
-    def getName(self):
-        return self.name
-    
-    def setName(self,name):
-        self.name=name
-    
-    def getSalary(self):
-        return self.salary
-    
-    def setSalary(self,salary):
-        if salary>0:
-            self.salary=salary
-        else:
-            print("Salary must be greater than 0")
-        return self.salary    
-
-    def getJobtitle(self):
-        return self.jobtitle
-    
-    def setJobtitle(self,jobtitle):
-        self.jobtitle=jobtitle         
-
-
--- 02_DS/00_objects/main.py
+++
@@ -1,6 +0,0 @@
-from Employee import Employee
-
-test=Employee("Raj","Analyst",45000)
-print(test.getName())
-print(test.getJobtitle())
-print(test.getSalary())
