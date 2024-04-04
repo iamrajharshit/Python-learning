98e67f83e6cd568adf083eae874e30e1eb4d77b0
Harshit Raj <iamrajharshit@gmail.com>
2024-04-05 01:03:44 +0530

    linked list

---
+++ 02_DS/04_Linked List/node.py
@@ -0,0 +1,19 @@
+class Node:
+
+    def __init__(self,value,next):
+        self.value=value
+        self.next=next
+
+    def getValue(self):
+        return self.value
+    
+    def getNext(self):
+        return self.next
+    
+    def setValue(self,value):
+        self.value=value
+
+    def setNext(self,next):
+        self.next=next
+
+    
