da7232c4e30fd3c5dfc5dd1aab6ae4c703ea2f41
Harshit Raj <iamrajharshit@gmail.com>
2024-06-08 10:30:41 +0530

    placement training with Dlithe

--- Dlithe/05_sets.py
+++ Dlithe/05_sets.py
@@ -9,11 +9,28 @@ def find_vov(str1):
         if i in {'a','e','i','o','u'}:
             c=c+1
     return c        
-print(find_vov(str1))    
+#print(find_vov(str1))    
 
 ###################################2#################################
 
 '''
 python prog to accept the strings which contains all vovles
 '''
+str2= "I am Raj you knew it right"
 
+def all_vov(str2):
+    for i in {'a','e','i','o','u'}:
+        if i in str2.lower():
+
+            return True
+        else:
+            return False
+
+print(all_vov(str2))
+
+
+#########################################3###########################
+
+'''
+
+'''
